ffd6444eb5ebfcdc226499004e9a9bd1c9e13235
renatoganske <renatoganske.jr@gmail.com>
2022-05-02 02:18:27 -0300

    feature: criação da classe DAO e conexão com banco de dados
    Criação da classe DAO, configuração e teste da conexão com o banco de dados.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.DAO;
 
 /**
  * Servlet implementation class Controller
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns={"/Controller", "/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	DAO dao = new DAO();
 
     /**
      * Default constructor. 
@@ -27,6 +29,8 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//teste de conexão
+		dao.testeConexao();
 	}
 
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,38 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/** Módulo de conexão **/
+	
+	//Parâmetros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
+	private String password = "1234";
+	
+	//Método de conexão
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	//teste de conexão
+	public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
