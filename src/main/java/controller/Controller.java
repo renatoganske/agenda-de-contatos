a903f99191665a57204f6da42ce5fe3735d648fd
renatoganske <renatoganske.jr@gmail.com>
2022-05-02 02:37:35 -0300

    feature: Configuração da camada Controller(modelo MVC).
    Configuração da camada Controller(modelo MVC) e do Servlet para encaminhamento das requisições.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,29 +8,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns={"/Controller", "/main"})
+@WebServlet(urlPatterns = { "/Controller", "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
 
-    /**
-     * Default constructor. 
-     */
-    public Controller() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		//teste de conexão
-		dao.testeConexao();
+	public Controller() {
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		System.out.println(action);
+		if (action.equals("/main")) {
+			contatos(request, response);
+		}
+	}
+
+	// Listar contatos
+	protected void contatos (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+		
+	}
 }
