30229ca400a77a1dbd70cc6bea494068e074e0a0
renatoganske <renatoganske.jr@gmail.com>
2022-05-02 23:02:40 -0300

    feature: impementada a submissão dos dados do formulário para a camada Model
    Implementada a submissão do formulário para a camada Model do modelo MVC, armazenando temporariamente os dados nas variáveis da classe JavaBeans.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 	}
@@ -19,15 +21,32 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String action = request.getServletPath();
-		System.out.println(action);
+
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if(action.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 
 	// Listar contatos
-	protected void contatos (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("agenda.jsp");
-		
+	protected void contatos (HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");		
+	}
+	
+	// Novo contato
+	protected void novoContato (HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//teste de recebimento dos dados do form
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("fone"));
+		System.out.println(request.getParameter("email"));
+		//setar as variáveis JavaBenas
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));		
+		contato.setEmail(request.getParameter("email"));		
 	}
 }
